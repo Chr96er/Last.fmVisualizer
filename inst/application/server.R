2ef8bab6ed18b9c68277fe54d744b8a3393f9394
Chris <christianhomberg@gmail.com>
2017-06-25 13:48:34 +1200

    - fix spello in manual

--- inst/application/server.R
+++ inst/application/server.R
@@ -28,7 +28,7 @@ function(input, output, session) {
   
   output$manual <- renderUI({
     manual(
-      "The Last.fmVisualizer application plots a user's top tags, artists, albums, and tracks as sunburst charts, using the last.fm API. For Artist, Artist-Album, and Artist-Track charts a contextmenu allows access to the selected element's Spotify and Last.fm link. Insert a last.fm user name or select a friend to get started.
+      "The Last.fmVisualizer application plots a user's top tags, artists, albums, and tracks as sunburst charts, using the last.fm API. For Artist, Artist-Album, and Artist-Track charts a context menu allows access to the selected element's Spotify and Last.fm link. Insert a last.fm user name or select a friend to get started.
       Note: Since 29/05/17 Spotify requires access tokens to all Web API Endpoints. Spotify integration is therefore not possible until further notice."
     )
   })
